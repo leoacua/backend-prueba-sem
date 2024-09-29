25dede434d7b8d8af8c7a248e8103c39828af524
Leonardo Acuña V <Leonardoacuavilla@gmail.com>
2024-09-29 08:07:59 -0500

    05- se realiza prueba de get para consumir data 29-09-2024

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const authRoutes = require('./routes/authRoutes');
 const tareahRoutes = require('./routes/tareas.js');
 const express = require('express');
 const conectarDB = require('./config/db');
@@ -15,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas de la API
-app.use('/api/tareas', require('./routes/tareas'));
+app.use('/api/tareas',tareahRoutes);
 
 const PUERTO = process.env.PORT || 5000;
 app.listen(PUERTO, () => console.log(`Servidor corriendo en el puerto ${PUERTO}`));
--- models/Tarea.js
+++ models/Tarea.js
@@ -1,33 +1,26 @@
 const mongoose = require('mongoose');
 
-const TaskSchema = new mongoose.Schema({
-  titulo: {
-     type: String,
-     required: [true, 'El título es obligatorio'],
-  },
-  descripcion: {
-     type: String,
-  },
-   prioridad: {
-    type: String,
-    enum: ['Alta', 'Media', 'Baja'],
-    default: 'Media',
-  },
-   fecha_limite: {
-    type: Date,
-  },
-  estado: {
-    type: String,
-    enum: ['Pendiente', 'En Progreso', 'Completada'],
-    default: 'Pendiente',
-  },
-  asignado_a: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Debe asignarse a un usuario'],
-  },
-}, {
-  timestamps: true,
+// Definición del esquema de la tarea
+const TareaSchema = new mongoose.Schema({
+    titulo: { type: String, required: true },
+    descripcion: { type: String, required: true },
+    prioridad: { type: String, enum: ['Baja', 'Media', 'Alta'], default: 'Baja' },
+    estado: { type: String, enum: ['Pendiente', 'En Progreso', 'Completada'], default: 'Pendiente' },
+    asignadoA: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' }, // Referencia a la colección de Usuarios
+    proyectoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Proyecto' }, // Referencia a la colección de Proyectos
+    fechaLimite: { type: Date },
+    creadoEn: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Task', TaskSchema);
+// Exportar el modelo Tarea
+module.exports = mongoose.model('Tarea', TareaSchema);
+
+const ProyectoSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    descripcion: { type: String },
+    fecha_inicio: { type: Date },
+    fecha_fin: { type: Date },
+    tareas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tarea' }] // Lista de tareas relacionadas
+  });
+  
+  module.exports = mongoose.model('Proyecto', ProyectoSchema);
